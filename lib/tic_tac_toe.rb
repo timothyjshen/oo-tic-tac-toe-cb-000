f04176a230114e6950f84ecc599dfbb88f260bcc
observant-communicator-1765 <timothyjshen@gmail.com>
2019-06-24 16:48:38 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -69,8 +69,8 @@ class TicTacToe
   def won?
     WIN_COMBINATIONS.detect do |combo|
       @board[combo[0]] == @board[combo[1]] &&
-        @board[combo[1]] == @board[combo[2]] &&
-        position_taken?(combo[0])
+      @board[combo[1]] == @board[combo[2]] &&
+      position_taken?(combo[0])
     end
   end
 
